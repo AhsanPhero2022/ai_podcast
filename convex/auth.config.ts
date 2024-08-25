e170363447cafbba333484d0cc01894d29bb1c9e
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-08-25 20:34:16 +0600

    clerk middleware setup done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://helpful-bobcat-64.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
